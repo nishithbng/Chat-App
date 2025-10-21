19e6a34a224c5c9ce4507536a2fbc283ab72b880
Nishith Bhat <nishithbhat69@gmail.com>
2025-10-21 09:41:31 +0530

    added images to public folder

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,12 +5,11 @@ import LoginPage from './pages/LoginPage'
 import ProfilePage from './pages/ProfilePage'
 import { Toaster } from "react-hot-toast"
 import { AuthContext } from '../context/AuthContext'
-import bgImage from "./assets/bgImage.svg";
 
 const App = () => {
   const {authUser} = useContext(AuthContext)
   return (
-    <div className="bg-contain"  style={{ backgroundImage: `url(${bgImage})` }}>
+    <div className="bg-contain"  style={{ backgroundImage: `url('/bgImage.svg')` }}>
       <Toaster />
       <Routes>
       <Route path='/' element={authUser ? <HomePage /> : <Navigate to="/login"/>}/>
